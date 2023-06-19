3d8916ac4e43e66196903b9dda1640891171e241
blubrrymuffin <123631700+blubrrymuffin@users.noreply.github.com>
2023-06-19 23:16:25 +0800

    Add files via upload

---
+++ ActivityOOP/OOP Activity/Home/Form2.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace OOP_Activity
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.btnproductlist = new System.Windows.Forms.Button();
+            this.btncategory = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnproductlist
+            // 
+            this.btnproductlist.BackColor = System.Drawing.Color.Transparent;
+            this.btnproductlist.FlatAppearance.BorderSize = 0;
+            this.btnproductlist.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(197)))), ((int)(((byte)(129)))));
+            this.btnproductlist.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnproductlist.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnproductlist.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnproductlist.Image = ((System.Drawing.Image)(resources.GetObject("btnproductlist.Image")));
+            this.btnproductlist.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnproductlist.Location = new System.Drawing.Point(0, 234);
+            this.btnproductlist.Name = "btnproductlist";
+            this.btnproductlist.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnproductlist.Size = new System.Drawing.Size(186, 43);
+            this.btnproductlist.TabIndex = 0;
+            this.btnproductlist.Text = "Product List";
+            this.btnproductlist.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnproductlist.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnproductlist.UseVisualStyleBackColor = false;
+            this.btnproductlist.Click += new System.EventHandler(this.btnproductlist_Click);
+            // 
+            // btncategory
+            // 
+            this.btncategory.BackColor = System.Drawing.Color.Transparent;
+            this.btncategory.FlatAppearance.BorderSize = 0;
+            this.btncategory.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(197)))), ((int)(((byte)(129)))));
+            this.btncategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncategory.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncategory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btncategory.Image = ((System.Drawing.Image)(resources.GetObject("btncategory.Image")));
+            this.btncategory.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btncategory.Location = new System.Drawing.Point(0, 175);
+            this.btncategory.Name = "btncategory";
+            this.btncategory.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btncategory.Size = new System.Drawing.Size(186, 43);
+            this.btncategory.TabIndex = 1;
+            this.btncategory.Text = "Category";
+            this.btncategory.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btncategory.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btncategory.UseVisualStyleBackColor = false;
+            this.btncategory.Click += new System.EventHandler(this.btncategory_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(58)))), ((int)(((byte)(71)))));
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btncategory);
+            this.panel1.Controls.Add(this.btnproductlist);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 445);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 38);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(186, 116);
+            this.panel4.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(55, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(197)))), ((int)(((byte)(129)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 38);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(197)))), ((int)(((byte)(129)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(186, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(667, 38);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(186, 38);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(667, 407);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(0, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(667, 407);
+            this.panel6.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(853, 445);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OOP";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnproductlist;
+        private System.Windows.Forms.Button btncategory;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+    }
+}
---
+++ ActivityOOP/OOP Activity/Home/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Activity
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnproductlist_Click(object sender, EventArgs e)
+        {
+            Form1 product = new Form1();
+            product.ShowDialog();
+
+        }
+
+        private void btncategory_Click(object sender, EventArgs e)
+        {
+            CategoryForm category = new CategoryForm();
+            category.ShowDialog();
+        }
+    }
+}
